15571d8cc5f662614bf2a40439a10a6379544065
Yorick van Pelt <yorick@yorickvanpelt.nl>
2019-12-07 11:20:58 +0700

    Add check to make sure the hash is updated manually after niv update

--- default.nix
+++ default.nix
@@ -6,13 +6,16 @@ self: super:
 
 let
   sources = import ./nix/sources.nix;
+  version = builtins.substring 0 7 sources.ton.rev;
   src = self.fetchFromGitHub {
     inherit (sources.ton) owner repo rev;
-    # sadly, not the one niv comes up with
-    sha256 = "1znikk7l2pv5mdl9rh59dljdrqkbwnazlpdjr4yfc87bcynb1rbz";
+    # sadly, not the one niv comes up with. add this check
+    # to make sure people update the hash
+    sha256 = if version == "ceaed40" then
+      "1znikk7l2pv5mdl9rh59dljdrqkbwnazlpdjr4yfc87bcynb1rbz" else
+      "0000000000000000000000000000000000000000000000000000";
     fetchSubmodules = true;
   };
-  version = builtins.substring 0 7 sources.ton.rev;
 in
 
 {
