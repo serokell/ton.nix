bde47eeb868a97cef0f735b47400068ded964723
Yorick van Pelt <yorick@yorickvanpelt.nl>
2019-12-07 14:16:49 +0700

    add ton module, testing vm, flake file

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  edition = 201909;
+
+  description = "Nix tools for the TON blockchain";
+
+  outputs = { self, nixpkgs }:
+
+    let
+      supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+    in {
+
+      overlay = import ./.;
+
+      defaultPackage = forAllSystems (system:
+        (import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        }).ton);
+
+      checks =
+        forAllSystems (system: { build = self.defaultPackage.${system}; });
+
+      nixosModules.ton = {
+        nixpkgs.overlays = [ self.overlay ];
+        imports = [ ./modules/ton.nix ];
+      };
+    };
+}
---
+++ modules/virtual.nix
@@ -0,0 +1,34 @@
+{
+  services.openssh.enable = true;
+  # install the vm-ssh.key.pub for ssh access
+  users.users.root.openssh.authorizedKeys.keyFiles = [ ../vm-ssh.key.pub ];
+  services.mingetty = {
+    autologinUser = "root";
+    helpLine = ''
+      ssh access:
+        ./result/bin/ssh
+      update running system (does not survive reboot):
+        nix-build -A vm && ./result/bin/switch-running-vm
+      website:
+        http://localhost:8080/
+      to get out:
+        poweroff
+    '';
+  };
+  # qemu settings:
+  virtualisation = {
+    memorySize = 1024;
+    diskSize = 1024;
+    # set up serial console
+    graphics = false;
+    qemu = {
+      options = [ "-serial mon:stdio" ];
+      # forward port 22 to 2222 and port 80 to 8080
+      # based on the default in nixpkgs
+      networkingOptions = [
+        "-net nic,netdev=user.0,model=virtio"
+        "-netdev user,id=user.0,hostfwd=tcp::2222-:22,hostfwd=tcp::8080-:80\${QEMU_NET_OPTS:+,$QEMU_NET_OPTS}"
+      ];
+    };
+  };
+}
